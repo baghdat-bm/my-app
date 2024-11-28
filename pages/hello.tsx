52d773f56eabc6425f1fdef0c4830285ff4c147c
baghdat-bm <baghdat.bm@gmail.com>
2024-11-28 23:28:18 +0500

    достигнута страница 87

--- pages/hello.tsx
+++ pages/hello.tsx
@@ -1,8 +1,32 @@
 import type { NextPage } from "next";
+import Head from "next/head";
+import Link from "next/link";
+import Image from "next/image";
 
 const Hello: NextPage = () => {
 
-    return (<>Hello World!</>);
+    return (
+        <>
+            <Head>
+                <title>Hello World Page Title</title>
+                <meta property="og:title" content="Hello World" key="title" />
+            </Head>
+            <h1>Hello World!</h1>
+            <p>
+                Use the HTML anchor for an
+                <a href="https://nostarch.com" > external link</a>
+                {' '} and the Link component for an
+                <Link href="/components/weather"> internal page
+                </Link>.
+                <Image
+                    src="/vercel.svg"
+                    alt="Vercel Logo"
+                    width={72}
+                    height={16}
+                />
+            </p>
+        </>
+    );
 }
 
 export default Hello;
---
+++ pages/names-ssr.tsx
@@ -0,0 +1,46 @@
+import type {
+    GetServerSideProps,
+    // GetServerSidePropsContext,
+    InferGetServerSidePropsType,
+    NextPage,
+    // PreviewData
+} from "next";
+// import { ParsedUrlQuery } from "querystring";
+import { fetchNames } from "../utils/fetch-names";
+
+type responseItemType = {
+    id: string;
+    name: string;
+};
+
+const NamesSSR: NextPage = (props: InferGetServerSidePropsType<typeof getServerSideProps>) => {    
+    const output = props.names.map((item: responseItemType, idx: number) => {
+        return (
+            <li key={`name-${idx}`}>
+                {item.id} : {item.name}
+            </li>
+        );
+    });
+    return (
+        <ul>
+            {output}
+        </ul>
+    );
+};
+
+export const getServerSideProps: GetServerSideProps = async (
+) => {
+    let names: responseItemType[] | [] = [];
+    try {
+        names = await fetchNames();
+    } catch (err) { 
+        console.error(err);
+    }
+    return {
+        props: {
+            names
+        }
+    };
+};
+
+export default NamesSSR;
